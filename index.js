4f809fced685e49bcb772c876d9674d3925b7ad6
James McConnaughey <jbmcconnaughey@gmail.com>
2023-04-15 13:43:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer"); //include inquierer package
+const fs = require("fs"); //include the file system package
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
