dd2110406b288f4e2504f5a718e97cf5862a9761
James McConnaughey <jbmcconnaughey@gmail.com>
2023-04-15 14:05:15 -0400

    added questions for the README generator

--- index.js
+++ index.js
@@ -3,13 +3,76 @@ const inquirer = require("inquirer"); //include inquierer package
 const fs = require("fs"); //include the file system package
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+//enter project title
+{
+type: 'input',
+name: 'title',
+message: 'Enter the project title:',
+},
+//enter description
+{
+type: 'input',
+name: 'description',
+message: 'Enter the project/application description:',
+},
+//enter installation instructions
+{
+type: 'input',
+name: 'installation',
+message: 'Enter the installation instructions for the application:',
+},
+//enter usage information
+{
+type: 'input',
+name: 'usage',
+message: 'Enter usage information for the application:',    
+},
+//enter contribution guidelines
+{
+type: 'input',
+name: 'contribution',
+message: 'Enter any guidelines for contributors to the project:',    
+},
+//enter test instructions
+{
+type: 'input',
+name: 'test',
+message: 'Enter test instructions for the application:',    
+},
+//choose a license
+{
+type: 'checkbox',
+name: 'license',
+message: 'Enter test instructions for the application:',    
+},
+//enter your github username
+{
+type: 'input',
+name: 'username',
+message: 'Enter your Github username:',    
+},
+//enter email address
+{
+type: 'input',
+name: 'email',
+message: 'Enter your email address:',    
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            
+
+        }
+        )
+}
 
 // Function call to initialize app
 init();
